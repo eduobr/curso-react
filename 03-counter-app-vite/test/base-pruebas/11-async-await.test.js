0fd651808f6842d092c0338e88d47a37666001ef
eduobr <eduardo.obreque@femsasalud.com>
2024-08-05 18:40:32 -0400

    added library testing for react

--- 03-counter-app-vite/test/base-pruebas/11-async-await.test.js
+++ 03-counter-app-vite/test/base-pruebas/11-async-await.test.js
@@ -1,4 +1,4 @@
-import { getImagen } from "../src/base-pruebas/11-async-await";
+import { getImagen } from "../../src/base-pruebas/11-async-await";
 
 describe('Pruebas en 11-async-await', () => {
     test('getImagen debe retornar un URL de la imagen', async () => {
